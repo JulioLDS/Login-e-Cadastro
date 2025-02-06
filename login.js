5810d15e8305d04656f9153456b790898a9d9792
Julio Lima da Silva <117543553+JulioLDS@users.noreply.github.com>
2025-02-05 22:03:18 -0300

    upload dos arquivos

---
+++ login.js
@@ -0,0 +1,12 @@
+// Seleciona os elementos de login, register e os botões
+const registerButton = document.getElementById("register");
+const loginButton = document.getElementById("login");
+const container = document.getElementById("container");
+
+registerButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+loginButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
